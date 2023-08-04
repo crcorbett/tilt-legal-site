803b5090a42989136e41ada8b6868e988df5da7f
Cooper Corbett <coopercorbett@gmail.com>
2023-08-04 23:26:06 +1000

    🎉 Initialise repository

---
+++ next.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
+ * This is especially useful for Docker builds.
+ */
+!process.env.SKIP_ENV_VALIDATION && import("./src/env/env.mjs");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  swcMinify: true,
+};
+
+module.exports = nextConfig;
