9a7b5b5384e22523daaadc9b662d69dece0a59f3
Cooper Corbett <coopercorbett@gmail.com>
2023-08-04 23:56:17 +1000

    🍱 Add CalSans-SemiBold font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/ThemeProvider";
+
+const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const fontHeading = localFont({
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+});
 
 export const metadata = {
   themeColor: [
@@ -17,7 +24,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={cn("bg-background min-h-screen", inter.className)}>
+      <body
+        className={cn(
+          "bg-background min-h-screen font-sans antialiased",
+          fontSans.variable,
+          fontHeading.variable
+        )}
+      >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
