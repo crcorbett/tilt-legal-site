d5b93392c5ff463342232d1e0f6a2430f9428a28
Cooper Corbett <coopercorbett@gmail.com>
2023-08-06 18:17:34 +1000

    🎨 Add barrel imports

---
+++ src/app/blog/(components)/index.ts
@@ -0,0 +1,6 @@
+import { Heading } from "./Heading";
+import { Callout } from "./Callout";
+import { TableOfContents } from "./TableOfContents";
+import { Paragraph } from "./Paragraph";
+
+export { Heading, Callout, TableOfContents, Paragraph };
