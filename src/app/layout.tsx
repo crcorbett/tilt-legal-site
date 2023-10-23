ef9139e034cfb4b6b235b443e992456ab5280707
Cooper Corbett <coopercorbett@gmail.com>
2023-10-23 12:59:06 +1100

    🐛 Fix bugs with children in page.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import localFont from "next/font/local";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { SiteFooter } from "@/components/SiteFooter";
 
 const fontSans = Inter({ subsets: ["latin"], variable: "--font-sans" });
 const fontHeading = localFont({
@@ -37,7 +40,26 @@ export default function RootLayout({
           enableSystem
           themes={["light", "dark", "black"]}
         >
-          {children}
+          <div className="flex flex-col min-h-screen">
+            <header className="container z-40 bg-background">
+              <div className="flex items-center justify-between h-20 py-6">
+                {/* <MainNav items={marketingConfig.mainNav} /> */}
+                <nav>
+                  <Link
+                    href="/login"
+                    className={cn(
+                      buttonVariants({ variant: "secondary", size: "sm" }),
+                      "px-4"
+                    )}
+                  >
+                    {`Legal Tech Map 2023`}
+                  </Link>
+                </nav>
+              </div>
+            </header>
+            <main className="flex-1">{children}</main>
+            <SiteFooter />
+          </div>
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,3 @@
-import Link from "next/link";
-
-// import { marketingConfig } from "@/config/marketing";
-import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
-// import { MainNav } from "@/components/main-nav";
-import { SiteFooter } from "@/components/SiteFooter";
-
-interface MarketingLayoutProps {
-  children: React.ReactNode;
-}
-
-export default async function MainLayout({ children }: MarketingLayoutProps) {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <header className="container z-40 bg-background">
-        <div className="flex items-center justify-between h-20 py-6">
-          {/* <MainNav items={marketingConfig.mainNav} /> */}
-          <nav>
-            <Link
-              href="/login"
-              className={cn(
-                buttonVariants({ variant: "secondary", size: "sm" }),
-                "px-4"
-              )}
-            >
-              {`Legal Tech Map 2023`}
-            </Link>
-          </nav>
-        </div>
-      </header>
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
-    </div>
-  );
+export default function MainLayout() {
+  return <div></div>;
 }
